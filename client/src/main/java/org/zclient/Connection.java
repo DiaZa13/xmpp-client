f8e453ca6409dfd16ccfe31ba6577ca7ae3501b2
zaray <cor191025@uvg.edu.gt>
2022-08-11 23:50:49 -0600

    adding listener to every presence sent by the server

--- client/src/main/java/org/zclient/Connection.java
+++ client/src/main/java/org/zclient/Connection.java
@@ -1,10 +1,11 @@
 package org.zclient;
 
 
-import org.jivesoftware.smack.AbstractXMPPConnection;
-import org.jivesoftware.smack.ConnectionConfiguration;
-import org.jivesoftware.smack.SmackException;
-import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.*;
+import org.jivesoftware.smack.filter.StanzaFilter;
+import org.jivesoftware.smack.filter.StanzaTypeFilter;
+import org.jivesoftware.smack.packet.Message;
+import org.jivesoftware.smack.packet.Presence;
 import org.jivesoftware.smack.tcp.XMPPTCPConnection;
 import org.jivesoftware.smack.tcp.XMPPTCPConnectionConfiguration;
 
@@ -16,11 +17,9 @@ public class Connection {
 
     public Connection(String domain){
         this.domain = domain;
-
     }
 
     public void start() {
-
         try {
             XMPPTCPConnectionConfiguration configuration = XMPPTCPConnectionConfiguration.builder()
                     .setXmppDomain(domain)
@@ -36,13 +35,36 @@ public class Connection {
         } catch (SmackException | IOException | XMPPException | InterruptedException e) {
             throw new RuntimeException(e);
         }
-
     }
 
     public AbstractXMPPConnection getStream() {
         return stream;
     }
 
+    public void presenceListener(){
+        StanzaListener listener = stanza -> {
+            Presence presence = (Presence) stanza;
+            System.out.printf("%s, presence: %s -> %s \n",presence.getFrom(), presence.getType(), presence.getStatus());
+        };
+
+
+        StanzaFilter presence_filter = new StanzaTypeFilter(Presence.class);
+
+        stream.addStanzaListener(listener, presence_filter);
+    }
+
+    public void messageListener(){
+        StanzaListener listener = stanza -> {
+            Message message = (Message) stanza;
+            System.out.printf("%s:, %s \n",message.getFrom(), message.getBody());
+        };
+
+        StanzaFilter message_filter = new StanzaTypeFilter(Message.class);
+
+        stream.addStanzaListener(listener, message_filter);
+    }
+
+    // maybe i have to remove the listeners
     public void close(){
         stream.disconnect();
     }
