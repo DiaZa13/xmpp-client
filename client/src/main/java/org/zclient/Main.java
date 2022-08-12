3d1fdb8edfab39ee7fb59a6a364c0668c8f04073
zaray <cor191025@uvg.edu.gt>
2022-08-12 14:09:38 -0600

    changing come texto color

--- client/src/main/java/org/zclient/Main.java
+++ client/src/main/java/org/zclient/Main.java
@@ -31,7 +31,7 @@ public class Main {
 
                     System.out.print("Username: ");
                     username = read.nextLine();
-                    System.out.print("Password:");
+                    System.out.print("Password: ");
                     password = read.nextLine();
                     User user = new User(username, password);
                     authentication.singIn(user);
@@ -52,14 +52,14 @@ public class Main {
                     do{
                         auth_opt = read.nextLine();
                         if (auth_opt.equals("-help")){
-                                System.out.println("-users\t show users/contacts");
-                                System.out.println("-add<username, email> \t add a user to contacts");
-                                System.out.println("-details<user jid> \t details of a user");
-                                System.out.println("-msg<user/group jid, msg/file> \t sends a message o file to a group or user");
-                                System.out.println("-join<room@service/nickname> \t join a chat room");
-                                System.out.println("-presence<new presence> \t edit user profile");
-                                System.out.println("-delete \t delete the account");
-                                System.out.println("-logout \t logout");
+                                System.out.println("\t\033[0;37m-users                            show users/contacts");
+                                System.out.println("\t-add<username, email>             add a user to contacts");
+                                System.out.println("\t-details<user jid>                details of a user");
+                                System.out.println("\t-msg<user/group jid, msg/file>    sends a message o file to a group or user");
+                                System.out.println("\t-join<room@service/nickname>      join a chat room");
+                                System.out.println("\t-presence<new presence>           edit user profile");
+                                System.out.println("\t-delete                           delete the actual logged account");
+                                System.out.println("\t-logout                           logout \033[0m");
                         } else if (auth_opt.startsWith("-users")) {
                             contacts.getContacts();
 
