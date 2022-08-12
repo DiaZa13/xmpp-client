b83d41e1bc000b4b77fb5c4b9500c0f13b2f9f5e
zaray <cor191025@uvg.edu.gt>
2022-08-12 01:33:08 -0600

    a user can edit its presence message

--- client/src/main/java/org/zclient/User.java
+++ client/src/main/java/org/zclient/User.java
@@ -1,10 +1,29 @@
 package org.zclient;
 
+import org.jivesoftware.smack.AbstractXMPPConnection;
+import org.jivesoftware.smack.SmackException;
+import org.jivesoftware.smack.packet.Message;
+import org.jivesoftware.smack.packet.Presence;
+import org.jxmpp.jid.impl.JidCreate;
+
 public record User(String username, String password) {
     public User {
         if (username.isEmpty() || password.isEmpty()) {
             throw new java.lang.IllegalArgumentException(
                     String.format("Invalid arguments: %s, %s", username, password));
+        }
+    }
+
+    public void editPresence(AbstractXMPPConnection connection, String status){
+        Presence presence = connection.getStanzaFactory()
+                .buildPresenceStanza()
+                .setStatus(status)
+                .build();
+
+        try {
+            connection.sendStanza(presence);
+        } catch (SmackException.NotConnectedException | InterruptedException e) {
+            throw new RuntimeException(e);
         }
     }
 }
