247c6af72aa06457c2ac02d0dc3329556bf5b641
zaray <cor191025@uvg.edu.gt>
2022-08-11 18:30:41 -0600

    joining and sending a message to a group

--- client/src/main/java/org/zclient/Communication.java
+++ client/src/main/java/org/zclient/Communication.java
@@ -6,25 +6,52 @@ import org.jivesoftware.smack.chat2.Chat;
 import org.jivesoftware.smack.chat2.ChatManager;
 import org.jivesoftware.smack.chat2.IncomingChatMessageListener;
 import org.jivesoftware.smack.packet.Message;
+import org.jivesoftware.smack.packet.Presence;
+import org.jivesoftware.smackx.muc.MultiUserChat;
+import org.jivesoftware.smackx.muc.MultiUserChatManager;
 import org.jxmpp.jid.EntityBareJid;
+import org.jxmpp.jid.Jid;
 import org.jxmpp.jid.impl.JidCreate;
 import org.jxmpp.stringprep.XmppStringprepException;
 
 public class Communication {
 
-    private ChatManager chatManager;
+    private final ChatManager chatManager;
     private String income_message;
+    private final AbstractXMPPConnection connection;
     private Boolean new_message = false;
 
     public Communication(AbstractXMPPConnection connection){
         chatManager = ChatManager.getInstanceFor(connection);
+        this.connection = connection;
     }
 
-    public void sendDirectmessage(String to, String msg){
+    public void sendMessage(String to, String msg, Boolean group){
         try {
-            Chat chat = chatManager.chatWith(JidCreate.entityBareFrom(to));
-            // modify to also send files
-            chat.send(msg);
+
+            Message message;
+            if (!group){
+                // Send a direct message
+                Chat chat = chatManager.chatWith(JidCreate.entityBareFrom(to));
+                 message = connection.getStanzaFactory()
+                        .buildMessageStanza()
+                        .ofType(Message.Type.chat)
+                        .setBody(msg)
+                        .build();
+                chat.send(message);
+            }else{
+                // Send a group message
+                message = connection.getStanzaFactory()
+                        .buildMessageStanza()
+                        .ofType(Message.Type.groupchat)
+                        .to(JidCreate.from(to))
+                        .setBody(msg)
+                        .build();
+                connection.sendStanza(message);
+            }
+
+
+            // TODO send files
 
         } catch (XmppStringprepException | SmackException.NotConnectedException | InterruptedException e) {
             throw new RuntimeException(e);
@@ -32,17 +59,23 @@ public class Communication {
 
     }
 
-    public void incomeListener(){
-        chatManager.addIncomingListener(new IncomingChatMessageListener() {
-            @Override
-            public void newIncomingMessage(EntityBareJid from, Message message, Chat chat) {
-                System.out.printf("%s: %s%n", from, message.getBody());
-                new_message = true;
-            }
-        });
-    }
-
     public void setNew_message(Boolean new_message) {
         this.new_message = new_message;
+    }
+
+    /*
+    * @param jid → describes the jid in the form <room@service/nickname>
+    */
+    public void joinGroup(String jid){
+        try {
+            Presence presence = connection.getStanzaFactory()
+                    .buildPresenceStanza()
+                    .to(JidCreate.from(jid))
+                    .build();
+
+            connection.sendStanza(presence);
+        } catch (XmppStringprepException | SmackException.NotConnectedException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
